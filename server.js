b4e3d9edc9c83484206c7342138788cac2dc97d9
damatgre <gregdamato2@gmail.com>
2021-06-28 11:10:25 -0400

    refactor for routes initial setup

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-//all required
+//all required dependencies
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
 
+//initialize express app 
 const app = express();
-
 //provide port
 const PORT = process.env.PORT || 3001;
+
 
 //parse incoming string or array data, converst to key/value pairing
 app.use(express.urlencoded({ extended: true }));
@@ -14,19 +15,25 @@ app.use(express.static("public"));
 //parse incoming JSON data
 app.use(express.json());
 
+
+
 //app get for notes
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, '/db/db.json'));
+    res.json(data);
+});
 
-    res.json()
-})
+app.post('/api/notes', (req, res) =>{
+
+});
 
 //set up port and console to show activity
 app.listen(PORT, () => {
